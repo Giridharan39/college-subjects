315f881544ec2a2272c13f7067085cd3a4e67d78
GIRIDHARAN G <126384652+Giridharan39@users.noreply.github.com>
2025-08-16 12:23:18 +0530

    Add files via upload

---
+++ fundamentals-of-computing/gg.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int input;
+    
+    printf("enter a number :");
+    scanf("%d",&input);
+    printf("the number is :%d",input);
+    return 0;
+}
